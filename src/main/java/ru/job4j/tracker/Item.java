62802db4ad7b29a2e9f83e71dd532f9e9afbc665
Vitas2048 <vithag97@mail.ru>
2022-03-24 22:43:37 +0700

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Item {
-    private LocalDateTime created = LocalDateTime.now() ;
+    private LocalDateTime created = LocalDateTime.now();
     private int id;
     private String name;
 
