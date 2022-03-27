31cf5c2b1e1ae19e547da39565774f3044aae5ab
Vitas2048 <vithag97@mail.ru>
2022-03-27 20:35:50 +0700

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -44,7 +44,7 @@ public class Tracker {
         int size = 0;
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            if (items[i] != null){
+            if (items[i] != null) {
                 if (items[i].getName().equals(key)) {
                     rsl[size] = item;
                     size++;
