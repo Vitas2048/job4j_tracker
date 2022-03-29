bf0a05b5bfad91bdfcdee94c26fa785cc227a383
Vitas2048 <vithag97@mail.ru>
2022-03-29 23:59:19 +0700

    2.2. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,8 +1,75 @@
 package ru.job4j.tracker;
 
+import java.util.Scanner;
+
 public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            Scanner input = new Scanner(System.in);
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            switch (select) {
+                case (0):
+                    System.out.println("Введите название заявки: ");
+                    String setName = input.nextLine();
+                    tracker.add(new Item()).setName(setName);
+                    System.out.print("Заявка добавлена \n");
+                break;
+                case (1):
+                    for (int i = 0; i < tracker.findAll().length; i++) {
+                        System.out.println(tracker.findAll()[i].toString());
+                    }
+                    break;
+                case (2):
+                    System.out.println("Введите id заявки которую хотите заменить");
+                    int change = Integer.parseInt(scanner.nextLine());
+                    tracker.replace(change, new Item());
+                    break;
+                case (3):
+                    System.out.println("Введите id заявки которую хотите удалить");
+                    int del = Integer.parseInt(scanner.nextLine());
+                    tracker.delete(del);
+                    break;
+                case (4):
+                    System.out.println("Введите id заявки которую хотите вывести на экран");
+                    int showid = Integer.parseInt(scanner.nextLine());
+                    System.out.println(tracker.findById(showid));
+                    break;
+                case (5):
+                    System.out.println("Введите имя заявки которую хотите вывести на экран");
+                    String showName = input.nextLine();
+                    for (int i = 0; i < tracker.findByName(showName).length; i++) {
+                        System.out.println(tracker.findByName(showName)[i].toString());
+                    }
+                    break;
+                case (6):
+                    run = false;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        Item notItem = new Item("Vitaly");
-        System.out.println(notItem);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
