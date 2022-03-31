299e719b93d90280776ba2e31230d020798e4417
Vitas2048 <vithag97@mail.ru>
2022-03-31 21:22:23 +0700

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #282320]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,84 +11,15 @@ public class StartUI {
             Scanner input = new Scanner(System.in);
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            switch (select) {
-                case (0):
-                    System.out.println("Введите название заявки: ");
-                    String setName = input.nextLine();
-                    tracker.add(new Item()).setName(setName);
-                    System.out.print("Заявка добавлена \n");
-                break;
-                case (1):
-                    for (int i = 0; i < tracker.findAll().length; i++) {
-                        System.out.println(tracker.findAll()[i].toString());
-                    }
-                    if (tracker.findAll().length == 0) {
-                        System.out.println("Хранилище не содержит заявок");
-                    }
-                    break;
-                case (2):
-                    if (tracker.findAll().length == 0) {
-                        System.out.println("Изменение заявки не удалось, т.к Хранилище не содержит заявок");
-                        break;
-                    }
-                    System.out.println("Введите id заявки которую хотите заменить");
-                    int change = Integer.parseInt(scanner.nextLine());
-                    System.out.println("Введите новое название заявки: ");
-                    String setName1 = input.nextLine();
-                    new Item().setName(setName1);
-                    Item b = new Item();
-                    b.setName(setName1);
-                    tracker.replace(change, b);
-                    if (!tracker.replace(change, b)) {
-                        System.out.println("ошибка замены");
-                    }
-                    break;
-                case (3):
-                    if (tracker.findAll().length == 0) {
-                        System.out.println("Удаление заявки не удалось, т.к хранилище не содержит заявок");
-                        break;
-                    }
-                    System.out.println("Введите id заявки которую хотите удалить");
-                    int del = Integer.parseInt(scanner.nextLine());
-                    tracker.delete(del);
-                    if (!tracker.delete(del)) {
-                        System.out.println("Заявка с таким id не найдена");
-                    }
-                    break;
-                case (4):
-                    if (tracker.findAll().length == 0) {
-                        System.out.println("Найти заявку не удалось, т.к. хранилище не содержит заявок ");
-                        break;
-                    }
-                    System.out.println("Введите id заявки которую хотите вывести на экран");
-                    int showid = Integer.parseInt(scanner.nextLine());
-
-                    if (tracker.findById(showid) == null) {
-                        System.out.println("Заявка с таким id не найдена");
-                    } else {
-                        System.out.println(tracker.findById(showid));
-                    }
-                    break;
-                case (5):
-                    if (tracker.findAll().length == 0) {
-                        System.out.println("Найти заявку не удалось, т.к. хранилище не содержит заявок ");
-                        break;
-                    }
-                    System.out.println("Введите имя заявки которую хотите вывести на экран");
-                    String showName = input.nextLine();
-                    for (int i = 0; i < tracker.findByName(showName).length; i++) {
-                        System.out.println(tracker.findByName(showName)[i].toString());
-                    }
-                    if (tracker.findByName(showName).length == 0) {
-                        System.out.println("Заявка с таким именем не найдена");
-                    }
-                    break;
-                case (6):
-                    run = false;
-                    break;
-                default:
-                    break;
-
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
+                run = false;
             }
         }
     }
