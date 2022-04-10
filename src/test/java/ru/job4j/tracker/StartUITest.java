57f7f589cd808e028c56907069b7148bcc34775b
Vitas2048 <vithag97@mail.ru>
2022-04-10 15:20:23 +0700

    9.2. Тесты вывода на консоль

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -101,7 +101,7 @@ public class StartUITest {
                         + "0. Find Item by Name " + ln
                         + "1. Exit Program" + ln
                         + "=== Find items by name ===" + ln
-                        + one.toString() + ln
+                        + one + ln
                         + "Menu." + ln
                         + "0. Find Item by Name " + ln
                         + "1. Exit Program" + ln
