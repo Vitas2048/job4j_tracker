826af7d5d49bc511218dfdf9b838f725ee8d988d
Vitas2048 <vithag97@mail.ru>
2022-04-19 21:28:03 +0700

    2. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/main/java/ru/job4j/tracker/search/PriorityQueue.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int index = 0;
+        int minPrior = 10;
+        for (Task element : tasks) {
+            if (minPrior < element.getPriority()) {
+                index++;
+            }
+            minPrior = element.getPriority();
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
