fcdb18c416eb557db7be7ebc22d8be57949e4ea9
Vitas2048 <vithag97@mail.ru>
2022-04-25 21:29:30 +0700

    3. Компаратор для строк [#35008 #293025]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,23 +6,15 @@ import java.util.List;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int r = 0, min = 0;
-        if (Integer.compare(left.length(), right.length()) <= 0) {
-            min = left.length();
-        } else {
-            min = right.length();
-        }
+        int result = 0;
+        int min = Math.min(left.length(), right.length());
         for (int i = 0; i < min; i++) {
-            if (Character.compare(left.charAt(i), right.charAt(i)) != 0) {
-                r = Character.compare(left.charAt(i), right.charAt(i));
+            result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0) {
                 break;
             }
-            if (Integer.compare(i, right.length() - 1) == 0 && Integer.compare(left.length(), right.length()) > 0) {
-                r = r + Character.compare(left.charAt(i + 1), Character.MIN_VALUE);
-            } else if (Integer.compare(i, left.length() - 1) == 0 && Integer.compare(right.length(), left.length()) > 0) {
-                r = r + Character.compare(Character.MIN_VALUE, right.charAt(i + 1));
-            }
         }
-        return r;
+
+        return result != 0 ? result : Integer.compare(left.length(), right.length());
     }
 }
