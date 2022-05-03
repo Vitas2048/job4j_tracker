5b040e6f343f7fb38a89072f5494010420bf7a20
Vitas2048 <vithag97@mail.ru>
2022-05-03 13:53:29 +0700

    2. Отсортировать департаменты [#34352 #296012]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
+        int k = 0;
         int rsl = 0;
         String[] s1 = o1.split("/");
         String[] s2 = o2.split("/");
@@ -12,8 +13,13 @@ public class DepDescComp implements Comparator<String> {
             for (String ss2 :s2) {
                 if (ss1.equals(ss2)) {
                     rsl = ss2.compareTo(ss1);
-                    break;
+                } else {
+                        k = 1;
+                        break;
                 }
+            }
+            if (k == 1) {
+                break;
             }
         }
         if (rsl == 0) {
