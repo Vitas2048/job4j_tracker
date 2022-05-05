d199556eb28b2d90b5c094d2b3a057d8b7a2f2bc
Vitas2048 <vithag97@mail.ru>
2022-05-06 00:01:11 +0700

    1.3. Встроенные функциональные интерфейсы. [#249209]

--- src/main/java/ru/job4j/function/StrategyUsage.java
+++ src/main/java/ru/job4j/function/StrategyUsage.java
@@ -19,6 +19,7 @@ public class StrategyUsage {
     public boolean check(Predicate<String> pred, String s) {
         return pred.test(s);
     }
+
     public String transform(Function<String, String> fun, String s) {
         return fun.apply(s);
     }
