966194e6589306d933cbec893117dd952ec2e38c
Vitas2048 <vithag97@mail.ru>
2022-05-09 17:25:06 +0700

    5. Подсчет функции в диапазоне. [#24251]

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -1,8 +1,22 @@
 package ru.job4j.lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntToDoubleFunction;
 import java.util.function.Supplier;
 
 public class ScopeInside {
+    public List<Double>diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> f  = new ArrayList<>();
+        Double x1 = 0D;
+        for (int i = start; i < end ; i++) {
+            double ii = i;
+            f.add(func.apply(ii));
+        }
+        return f;
+    }
+
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
