bd4e89e0b42e7dd588ae5eed05724166c6d8187e
Vitas2048 <vithag97@mail.ru>
2022-05-15 23:38:08 +0700

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -0,0 +1,44 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Tuple {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    private String name;
+
+    public double getScore() {
+        return score;
+    }
+
+    private double score;
+
+    public Tuple(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tuple tuple = (Tuple) o;
+        return Double.compare(tuple.score, score) == 0 &&
+                Objects.equals(name, tuple.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+}
