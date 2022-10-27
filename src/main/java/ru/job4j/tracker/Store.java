390e844ec82eb74371e03fb507b67da584d0a0c9
Vitas2048 <vithag97@mail.ru>
2022-10-27 19:58:16 +0700

    2. Трекер SQL [#1734]

--- src/main/java/ru/job4j/tracker/Store.java
+++ src/main/java/ru/job4j/tracker/Store.java
@@ -6,10 +6,17 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface Store {
+
     Item add(Item item) throws SQLException;
+
     boolean replace(int id, Item item) throws SQLException;
+
     boolean delete(int id) throws SQLException;
+
     List<Item> findAll() throws SQLException;
+
     List<Item> findByName(String key) throws SQLException;
+
     Item findById(int id) throws SQLException;
+
 }
