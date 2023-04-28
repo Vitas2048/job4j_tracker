4cc675b5a8947f6f8d73646a60eeab6aa1e5b0c7
vitas2048 <vithag97@mail.ru>
2023-04-28 18:43:22 +0700

    4. Docker сервисы [#504781]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,10 +6,14 @@ import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.input.ValidateInput;
 import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
-import ru.job4j.tracker.store.MemTracker;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 public class StartUI {
 
@@ -30,13 +34,40 @@ public class StartUI {
         }
     }
 
+    private static String loadSysEnvIfNullThenConfig(String sysEnv, String key, Properties config) {
+        String value = System.getenv(sysEnv);
+        if (value == null) {
+            value = config.getProperty(key);
+        }
+        return value;
+    }
+
+    private static Connection loadConnection() throws ClassNotFoundException, SQLException {
+        var config = new Properties();
+        try (InputStream in = StartUI.class.getClassLoader()
+                .getResourceAsStream("application.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        String url = loadSysEnvIfNullThenConfig("JDBC_URL", "url", config);
+        String username = loadSysEnvIfNullThenConfig("JDBC_USERNAME", "username", config);
+        String password = loadSysEnvIfNullThenConfig("JDBC_PASSWORD", "password", config);
+        String driver = loadSysEnvIfNullThenConfig("JDBC_DRIVER", "driver-class-name", config);
+        System.out.println("url=" + url);
+        Class.forName(driver);
+        return DriverManager.getConnection(
+                url, username, password
+        );
+    }
+
     public static void main(String[] args) {
         Input input = new ValidateInput(
                 new ConsoleInput()
         );
         Output output = new ConsoleOutput();
-        try (SqlTracker tracker = new SqlTracker()) {
-            tracker.init();
+        try (Connection connection = loadConnection()) {
+            Store tracker = new SqlTracker(connection);
             List<UserAction> actions = List.of(
                     new CreateAction(output),
                     new ReplaceAction(output),
@@ -44,11 +75,11 @@ public class StartUI {
                     new FindAllAction(output),
                     new FindByIdAction(output),
                     new FindByNameAction(output),
-                    new ExitAction()
-            );
+                    new ExitAction());
             new StartUI().init(input, tracker, actions);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
